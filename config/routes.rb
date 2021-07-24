61cf15d0881ab734c23f2c176e3cfcc3452ae906
EC2 Default User <ec2-user@ip-172-31-91-120.ec2.internal>
2021-07-24 06:39:34 +0000

    [Add]devise:admin・publicのモデル

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  #admin
+  devise_for :admins, controllers: {
+   sessions: "admins/sessions"
+ }
+  namespace :admins do
+    resources :homes, only: [:top]
+    resources :items, only: [:index, :new, :create, :show, :edit, :update,]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+  	resources :orders, only: [:show,:update]
+    resources :order_details, only: [:update]
+  end
+  #public
+  devise_for :publics, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+  }
+    root to: 'public/homes#top'
+    get 'about' => 'public/homes#about'
+    resources :items, only: [:index, :show]
+    resources :registrations, only: [:new, :create]
+    resources :sessions, only: [:new, :create, :destroy]
+    resources :customers, only: [:show, :edit, :update,]
+  end
+ 
