0f7c617534cffa01402b5bb2f15ebbaa18b70db4
EC2 Default User <ec2-user@ip-172-31-91-120.ec2.internal>
2021-08-21 01:47:31 +0000

    [Add]enums

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,6 @@ class Order < ApplicationRecord
   validates :postal_code, length: { is: 7 }, numericality: { only_integer: true }
   validates :shipping_cost, :total_payment, numericality: { only_integer: true }
 
-  enum payment_method: { credit: 0, bank: 1 }
+  enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { unpaid: 0, paid: 1, making: 2, preparation: 3, shipped: 4 }
 end
